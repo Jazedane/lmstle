5c86282890abdbee3897deb2f134d943071aa878
Jazedane <juzztinemanapao27@gmail.com>
2022-09-10 16:37:35 +0800

    commit message

--- bootstrap/js/bootstrap.js
+++ bootstrap/js/bootstrap.js
@@ -1,8 +1,8 @@
 /* ===================================================
- * bootstrap-transition.js v2.3.2
- * http://getbootstrap.com/2.3.2/javascript.html#transitions
+ * bootstrap-transition.js v4.6.2
+ * http://getbootstrap.com/4.6.2/javascript.html#transitions
  * ===================================================
- * Copyright 2013 Twitter, Inc.
+ * Copyright 2022 Twitter, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- header.php
+++ header.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html class="no-js">
-    <head>
-        <title> LMSTLE </title>
-		<meta name="viewport" content="width=device-width" />
-        <!-- Bootstrap -->
-			<!-- <link href="images/logo.png" rel="icon" type="image"> -->
-			<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-			<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-			<link href="fontawesome/css/all.css" rel="stylesheet" media="screen" type="text/css">
-			<link href="bootstrap/css/my_style.css" rel="stylesheet" media="screen">
-			<link href="vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
-			<link href="assets/styles.css" rel="stylesheet" media="screen">
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-		<!-- calendar css -->
-		<link href="vendors/fullcalendar/fullcalendar.css" rel="stylesheet" media="screen">
-		<script src="vendors/jquery-1.9.1.min.js"></script>
-        <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-		<!-- data table -->
-		<link href="assets/DT_bootstrap.css" rel="stylesheet" media="screen">
-		<!-- notification  -->
-		<link href="vendors/jGrowl/jquery.jgrowl.css" rel="stylesheet" media="screen">
-		<!-- wysiwug  -->
-	
-		<script src="vendors/jGrowl/jquery.jgrowl.js"></script>
-    </head>
+
+<head>
+    <title> LMSTLE </title>
+    <meta name="viewport" content="width=device-width" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha38-4-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+    <!-- Bootstrap -->
+
+    <link href="fontawesome/css/all.css" rel="stylesheet" media="screen" type="text/css">
+    <link href="vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+
+    <link href="vendors/fullcalendar/fullcalendar.css" rel="stylesheet" media="screen">
+    <script src="vendors/jquery-1.9.1.min.js"></script>
+    <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <!-- data table -->
+    <link href="assets/DT_bootstrap.css" rel="stylesheet" media="screen">
+    <!-- notification  -->
+    <link href="vendors/jGrowl/jquery.jgrowl.css" rel="stylesheet" media="screen">
+    <!-- wysiwug  -->
+
+    <script src="vendors/jGrowl/jquery.jgrowl.js"></script>
+</head>
 <?php include('dbcon.php'); ?>
