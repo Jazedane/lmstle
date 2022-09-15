b4abb9076c43046b49d9170d720d3f597ce6fc97
24thsaint <rave.arevalo@kingsland.io>
2022-09-16 00:37:08 +0800

    feat(logo): add transparent logo

--- title_index.php
+++ title_index.php
@@ -1,5 +1,5 @@
 <div class="row" style="margin-top: 50px">
-    <img class="rounded mx-auto d-block" src="images/logo.jpeg" style="width: 10rem;">
+    <img class="rounded mx-auto d-block" src="images/logo.png" style="width: 10rem;">
     <div class="title text-center">
         <h3>
             Learning Management System for TLE-Agriculture
