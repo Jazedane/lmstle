1a35b0f2a7d45da82007960e0db37ec7ad6b8b04
24thsaint <rave.arevalo@kingsland.io>
2022-09-17 21:50:33 +0800

    fix(student, header nav): fix script load order

--- header.php
+++ header.php
@@ -35,27 +35,6 @@
     <link href="vendors/datepicker.css" rel="stylesheet" media="screen">
     <link href="vendors/uniform.default.css" rel="stylesheet" media="screen">
     <link href="vendors/chosen.min.css" rel="stylesheet" media="screen">    
-
-    <script src="vendors/jquery-1.9.1.min.js"></script>
-    <script src="vendors/jquery.uniform.min.js"></script>
-
-    <script src="assets/DT_bootstrap.js"></script>
-    <script src="assets/jquery.hoverdir.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendors/bootstrap-datepicker.js"></script>
-    <script src="vendors/bootstrap-wysihtml5/lib/js/wysihtml5-0.3.0.js"></script>
-    <script src="vendors/bootstrap-wysihtml5/src/bootstrap-wysihtml5.js"></script>
-    <script src="vendors/chosen.jquery.min.js"></script>
-    <script src="vendors/ckeditor/adapters/jquery.js"></script>
-    <script src="vendors/ckeditor/ckeditor.js"></script>
-    <script src="vendors/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="vendors/easypiechart/jquery.easy-pie-chart.js"></script>
-    <script src="vendors/fullcalendar/fullcalendar.js"></script>
-    <script src="vendors/fullcalendar/gcal.js"></script>
-    <script src="vendors/jGrowl/jquery.jgrowl.js"></script>
-    <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    <script type="text/javascript" src="vendors/tinymce/js/tinymce/tinymce.min.js"></script>
-    <script src="assets/scripts.js"></script>
 </head>
 
 <?php include 'dbcon.php'; ?>
