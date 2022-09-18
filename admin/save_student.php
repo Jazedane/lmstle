f6be8a91ca85afb218fe251a90565d4d2a47d2a6
Jazedane <juzztinemanapao27@gmail.com>
2022-09-18 23:44:38 +0800

    feat(admin): change variable names

--- admin/save_student.php
+++ admin/save_student.php
@@ -6,8 +6,15 @@ include('dbcon.php');
                 $lastname = $_POST['lastname'];
                 $class_id = $_POST['class_id'];
                 $hashedPassword = hash('sha256', $lastname . $username);
+                
+mysqli_query($conn,
+    "insert into student (username,firstname,lastname,location,class_id,status,password)
+	values ('$username','$firstname','$lastname','uploads/NO-IMAGE-AVAILABLE.jpg','$class_id','Unregistered','$hashedPassword');"
+    ) or die(mysqli_error()); 
 
-               mysqli_query($conn,"insert into student (username,firstname,lastname,location,class_id,status,password)
-		values ('$username','$firstname','$lastname','uploads/NO-IMAGE-AVAILABLE.jpg','$class_id','Unregistered','$hashedPassword')                                    
-		") or die(mysqli_error()); 
+
+mysqli_query($conn,
+    "insert into teacher_class_student (teacher_class_id,student_id,teacher_id)
+	values ('15','$student_id','1234');"
+    ) or die(mysqli_error()); 
 ?>
