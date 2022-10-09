c4e8ae65304f28ed6f4a4a8084d8be10ff808b99
24thsaint <rave.arevalo@kingsland.io>
2022-10-09 23:42:10 +0800

    feat(admin): remove file upload for add task

--- admin/add_task.php
+++ admin/add_task.php
@@ -1,8 +1,8 @@
-<?php include('header.php'); ?>
-<?php include('session.php'); ?>
+<?php include 'header.php'; ?>
+<?php include 'session.php'; ?>
 
 <body id="class_div">
-    <?php include('sidebar.php'); ?>
+    <?php include 'sidebar.php'; ?>
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span10" id="content">
@@ -10,32 +10,33 @@
 
                     <ul class="breadcrumb">
                         <?php
-								$school_year_query = mysqli_query($conn,"select * from school_year order by school_year DESC")or die(mysqli_error());
-								$school_year_query_row = mysqli_fetch_array($school_year_query);
-								$school_year = $school_year_query_row['school_year'];
-								?>
+                        ($school_year_query = mysqli_query(
+                            $conn,
+                            'select * from school_year order by school_year DESC'
+                        )) or die(mysqli_error());
+                        $school_year_query_row = mysqli_fetch_array(
+                            $school_year_query
+                        );
+                        $school_year = $school_year_query_row['school_year'];
+                        ?>
                         <li><a href="#"><b>My Class</b></a><span class="divider">/</span></li>
-                        <li><a href="#">School Year: <?php echo $school_year_query_row['school_year']; ?></a></li>
+                        <li><a href="#">School Year: <?php echo $school_year_query_row[
+                            'school_year'
+                        ]; ?></a></li>
                     </ul>
 
                     <div class="block">
                         <div class="navbar navbar-inner block-header">
                             <div id="count_class" class="muted pull-right">
-
                             </div>
                         </div>
-                        <div class="block-content collapse in">
-                            <div class="span4">
-                                <form class="" id="add_downloadable" method="post" enctype="multipart/form-data"
-                                    name="upload">
+                        <form class="" id="add_downloadable" method="post" enctype="multipart/form-data" name="upload">
+                            <div class="block-content collapse in">
+                                <div class="span4">
                                     <div class="control-group">
                                         <label class="control-label" for="inputEmail">Task</label>
                                         <div class="mb-3">
-                                            <label for="formFileMultiple" class="form-label">
-                                                <input name="uploaded_file" class="form-control" type="file"
-                                                    id="formFileMultiple" multiple></input>
-                                                <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
-                                                <input type="hidden" name="id" value="<?php echo $session_id ?>" />
+                                            <input type="hidden" name="id" value="<?php echo $session_id; ?>" />
                                         </div>
                                     </div>
                                     <div class="control-group">
@@ -54,15 +55,24 @@
                                             <label for="">Status</label>
                                             <select name="status" id="status" class="custom-select custom-select-sm">
                                                 <option value="0"
-                                                    <?php echo isset($status) && $status == 0 ? 'selected' : '' ?>>
+                                                    <?php echo isset($status) &&
+                                                    $status == 0
+                                                        ? 'selected'
+                                                        : ''; ?>>
                                                     Pending
                                                 </option>
                                                 <option value="3"
-                                                    <?php echo isset($status) && $status == 3 ? 'selected' : '' ?>>
+                                                    <?php echo isset($status) &&
+                                                    $status == 3
+                                                        ? 'selected'
+                                                        : ''; ?>>
                                                     On-Hold
                                                 </option>
                                                 <option value="5"
-                                                    <?php echo isset($status) && $status == 5 ? 'selected' : '' ?>>
+                                                    <?php echo isset($status) &&
+                                                    $status == 5
+                                                        ? 'selected'
+                                                        : ''; ?>>
                                                     Done
                                                 </option>
                                             </select>
@@ -73,51 +83,28 @@
                                             <label for="" class="control-label">End Date</label>
                                             <input type="date" class="form-control form-control-sm" autocomplete="off"
                                                 name="end_date"
-                                                value="<?php echo isset($end_date) ? datetime("Y-m-d",strtotime($end_date)) : '' ?>"
+                                                value="<?php echo isset(
+                                                    $end_date
+                                                )
+                                                    ? datetime(
+                                                        'Y-m-d',
+                                                        strtotime($end_date)
+                                                    )
+                                                    : ''; ?>"
                                                 required>
                                         </div>
                                     </div>
-
-
-                                    <script>
-                                    jQuery(document).ready(function($) {
-                                        $("#add_downloadable").submit(function(e) {
-                                            $.jGrowl("Uploading File Please Wait......", {
-                                                sticky: true
-                                            });
-                                            e.preventDefault();
-                                            var _this = $(e.target);
-                                            var formData = new FormData($(this)[0]);
-                                            $.ajax({
-                                                type: "POST",
-                                                url: "add_task_save.php",
-                                                data: formData,
-                                                success: function(html) {
-                                                    $.jGrowl("Task Successfully Added", {
-                                                        header: 'Task Added'
-                                                    });
-                                                    window.location = 'add_task.php';
-                                                },
-                                                cache: false,
-                                                contentType: false,
-                                                processData: false
-                                            });
-                                        });
-                                    });
-                                    </script>
-
-
-                            </div>
-                            <div class="span8">
-                                <div class="alert alert-info">Check The Class you want to put this file.</div>
-                                <div class="">
-                                    Check All <input type="checkbox" name="selectAll" id="checkAll" />
-                                    <script>
-                                    $("#checkAll").click(function() {
-                                        $('input:checkbox').not(this).prop('checked', this.checked);
-                                    });
-                                    </script>
                                 </div>
+                                <div class="span8">
+                                    <div class="alert alert-info">Check The Class you want to put this file.</div>
+                                    <div class="">
+                                        Check All <input type="checkbox" name="selectAll" id="checkAll" />
+                                        <script>
+                                        $("#checkAll").click(function() {
+                                            $('input:checkbox').not(this).prop('checked', this.checked);
+                                        });
+                                        </script>
+                                    </div>
                                     <table cellpadding="0" cellspacing="0" border="0" class="table" id="example">
                                         <thead>
                                             <tr>
@@ -129,53 +116,89 @@
                                         </thead>
                                         <tbody>
 
-                                            <?php $query = mysqli_query($conn,"select * from teacher_class
+                                            <?php
+                                            ($query = mysqli_query(
+                                                $conn,
+                                                "select * from teacher_class
 										LEFT JOIN class ON class.class_id = teacher_class.class_id
 										LEFT JOIN subject ON subject.subject_id = teacher_class.subject_id
-										where teacher_id = '$session_id' and school_year = '$school_year' ")or die(mysqli_error());
-										$count = mysqli_num_rows($query);
-										
-									
-										while($row = mysqli_fetch_array($query)){
-										$id = $row['teacher_class_id'];
-				
-										?>
+										where teacher_id = '$session_id' and school_year = '$school_year' "
+                                            )) or die(mysqli_error());
+                                            $count = mysqli_num_rows($query);
+                                            while (
+                                                $row = mysqli_fetch_array(
+                                                    $query
+                                                )
+                                            ) {
+                                                $id =
+                                                    $row['teacher_class_id']; ?>
                                             <tr id="del<?php echo $id; ?>">
                                                 <td width="30">
                                                     <input id="" class="" name="selector[]" type="checkbox"
                                                         value="<?php echo $id; ?>">
                                                 </td>
-                                                <td><?php echo $row['class_name']; ?></td>
-                                                <td><?php echo $row['class_id']; ?></td>
+                                                <td><?php echo $row[
+                                                    'class_name'
+                                                ]; ?></td>
+                                                <td><?php echo $row[
+                                                    'class_id'
+                                                ]; ?></td>
                                             </tr>
 
-                                            <?php } ?>
-
-
-
+                                            <?php
+                                            }
+                                            ?>
                                         </tbody>
                                     </table>
-
-                            </div>
-                            <div class="span10">
-                                <center>
-                                    <div class="control-group">
-                                        <div class="controls">
-                                            <button name="Upload" type="submit" value="Upload"
-                                                class="btn btn-success" /><i class="fa-solid fa-upload"></i>
-                                            Upload</button>
+                                </div>
+                                <div class="span10">
+                                    <center>
+                                        <div class="control-group">
+                                            <div class="controls">
+                                                <button name="Upload" type="submit" value="Upload"
+                                                    class="btn btn-success">
+                                                    <i class="fa-solid fa-upload"></i>Upload
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
-                                </center>
+                                    </center>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-        <?php include('footer.php'); ?>
+        <?php include 'footer.php'; ?>
     </div>
-    <?php include('script.php'); ?>
+    <?php include 'script.php'; ?>
+    <script>
+    jQuery(document).ready(function($) {
+        $("#add_downloadable").submit(function(e) {
+            e.preventDefault();
+            $.jGrowl("Uploading File Please Wait......", {
+                sticky: true
+            });
+            e.preventDefault();
+            var _this = $(e.target);
+            var formData = new FormData($(this)[0]);
+            $.ajax({
+                type: "POST",
+                url: "add_task_save.php",
+                data: formData,
+                success: function(html) {
+                    $.jGrowl("Task Successfully Added", {
+                        header: 'Task Added'
+                    });
+                    window.location = 'add_task.php';
+                },
+                cache: false,
+                contentType: false,
+                processData: false
+            });
+        });
+    });
+    </script>
 </body>
 
 </html>
--- admin/add_task_save.php
+++ admin/add_task_save.php
@@ -6,82 +6,40 @@ $conn = $connector->DbConnector();
 $name = $_POST['name'];
 $filedesc = $_POST['desc'];
 
-$input_name = basename($_FILES['uploaded_file']['name']);
-echo $input_name;
-if ($input_name == '') {
-    $id = $_POST['selector'];
-    $N = count($id);
+$id = $_POST['selector'];
+$N = count($id);
 
-	$name_notification = "New Task Added: " . $name;
+$name_notification = 'New Task Added: ' . $name;
 
-    for ($i = 0; $i < $N; $i++) {
-        mysqli_query(
+for ($i = 0; $i < $N; $i++) {
+    mysqli_query(
+        $conn,
+        "INSERT INTO task (fdesc,fdatein,fname,teacher_id,class_id) VALUES ('$filedesc',NOW(),'$name','$session_id','$id[$i]')"
+    ) or die(mysqli_error());
+
+    ($teacher_class_query = mysqli_query(
+        $conn,
+        "SELECT * FROM teacher_class WHERE teacher_class_id = '$id[$i]'"
+    )) or die(mysqli_error());
+
+    $teacher_class_row = mysqli_fetch_array($teacher_class_query, 1);
+    $class_id = $teacher_class_row['class_id'];
+
+    ($student_query = mysqli_query(
+        $conn,
+        "SELECT * FROM student WHERE class_id = '$class_id'"
+    )) or die(mysqli_error());
+
+    while ($row = mysqli_fetch_array($student_query)) {
+        $student_id = $row['student_id'];
+
+        ($query = mysqli_query(
             $conn,
-            "INSERT INTO task (fdesc,fdatein,teacher_id,class_id) VALUES ('$filedesc',NOW(),'$session_id','$id[$i]')"
-        ) or die(mysqli_error());
-		
-		($teacher_class_query = mysqli_query(
-			$conn,
-			"SELECT * FROM teacher_class WHERE teacher_class_id = '$id[$i]'"
-		)) or die(mysqli_error());
-
-		$teacher_class_row = mysqli_fetch_array($teacher_class_query, 1);
-		$class_id = $teacher_class_row['class_id'];
-
-        ($student_query = mysqli_query(
-			$conn,
-			"SELECT * FROM student WHERE class_id = '$class_id'"
-		)) or die(mysqli_error());
-	
-		while ($row = mysqli_fetch_array($student_query)) {
-			$student_id = $row['student_id'];
-	
-			($query = mysqli_query(
-				$conn,
-				"INSERT INTO notification (broadcaster_id,receiver_id,message,link) VALUES ('$session_id','$student_id','$name_notification','task_student.php?id=" .
-					$id[$i] .
-					"')"
-			)) or die(mysqli_error());
-		}
-    }
-} else {
-    $rd2 = mt_rand(1000, 9999) . '_File';
-    $filename = basename($_FILES['uploaded_file']['name']);
-    $ext = substr($filename, strrpos($filename, '.') + 1);
-    $newname = 'uploads/' . $rd2 . '_' . $filename;
-    $name_notification = 'add task name' . ' ' . '<b>' . $name . '</b>';
-    move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $newname);
-    $id = $_POST['selector'];
-    $N = count($id);
-    for ($i = 0; $i < $N; $i++) {
-        mysqli_query(
-            $conn,
-            "INSERT INTO task (fdesc,floc,fdatein,teacher_id,fname,class_id) VALUES ('$filedesc','$newname',NOW(),'$session_id','$name','$id[$i]')"
-        ) or die(mysqli_error());
-
-		($teacher_class_query = mysqli_query(
-			$conn,
-			"SELECT * FROM teacher_class WHERE teacher_class_id = '$id[$i]'"
-		)) or die(mysqli_error());
-
-		$teacher_class_row = mysqli_fetch_array($teacher_class_query, 1);
-		$class_id = $teacher_class_row['class_id'];
-
-        ($student_query = mysqli_query(
-			$conn,
-			"SELECT * FROM student WHERE class_id = '$class_id'"
-		)) or die(mysqli_error());
-	
-		while ($row = mysqli_fetch_array($student_query)) {
-			$student_id = $row['student_id'];
-	
-			($query = mysqli_query(
-				$conn,
-				"INSERT INTO notification (broadcaster_id,receiver_id,message,link) VALUES ('$session_id','$student_id','$name_notification','task_student.php?id=" .
-					$id[$i] .
-					"')"
-			)) or die(mysqli_error());
-		}
+            "INSERT INTO notification (broadcaster_id,receiver_id,message,link) VALUES ('$session_id','$student_id','$name_notification','task_student.php?id=" .
+                $id[$i] .
+                "')"
+        )) or die(mysqli_error());
     }
 }
+
 ?>
