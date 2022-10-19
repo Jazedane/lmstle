ecf4186165a5357e434cad9aab74f0e24732d41a
24thsaint <rave.arevalo@kingsland.io>
2022-10-20 02:58:43 +0800

    fix(edit task): implement update interaction

--- admin/edit.php
+++ admin/edit.php
@@ -1,11 +1,13 @@
 <?php
 include('dbcon.php');
 include('session.php');
-$task_id = $_POST['id'];
+$student_task_id = $_POST['student_task_id'];
 $fname = $_POST['fname'];
 $fdesc = $_POST['fdesc'];
 $task_status = $_POST['task_status'];
+$p_condition = $_POST['p_condition'];
 
-mysqli_query($conn,"insert into task (task_id,fname,fdesc,task_status) 
-values('$task_id','$fname',NOW(),'$session_id','$fdesc','$task_status')")or die(mysqli_error());
+echo "UPDATE student_task SET fname='$fname',fdesc='$fdesc',task_status='$task_status',p_condition='$p_condition' WHERE student_task_id='$student_task_id'";
+
+$query = mysqli_query($conn,"UPDATE student_task SET fname='$fname',fdesc='$fdesc',task_status='$task_status',p_condition='$p_condition' WHERE student_task_id='$student_task_id'")or die(mysqli_error());
 ?>
