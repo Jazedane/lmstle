bac8c9c0c6040860644403f13035cb01848018c2
Jazedane <juzztinemanapao27@gmail.com>
2022-10-20 03:06:08 +0800

    merge

--- admin/edit.php
+++ admin/edit.php
@@ -5,7 +5,8 @@ $task_id = $_POST['id'];
 $fname = $_POST['fname'];
 $fdesc = $_POST['fdesc'];
 $task_status = $_POST['task_status'];
+$p_condition = $_POST['p_condition'];
 
-mysqli_query($conn,"insert into task (task_id,fname,fdesc,task_status) 
+mysqli_query($conn,"insert into task (task_id,fname,fdesc,task_status, p_condition) 
 values('$task_id','$fname',NOW(),'$session_id','$fdesc','$task_status')")or die(mysqli_error());
 ?>
