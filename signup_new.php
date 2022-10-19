ebce6f188f07885141d7307dd80e257d0228a75a
24thsaint <rave.arevalo@kingsland.io>
2022-10-20 03:10:10 +0800

    fix(signup new): implement teacher signup

--- signup_new.php
+++ signup_new.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'; ?>
+
 <body>
     <div class="container">
         <div class="row text-center">
@@ -8,7 +9,7 @@
             <div class="pull-center">
             </div>
         </div>
-    <?php include 'script.php'; ?>
+        <?php include 'script.php'; ?>
 </body>
 
 </html>
@@ -28,77 +29,83 @@
 </style>
 
 <div class="card">
-    <form method="post" id="login_form">
-        <div class="card-body text-center">
-            <div class="admin-index-form-size">
-                <h3 class="form-signin-heading"><i class="fa-solid fa-lock"></i> Sign-Up </h3>
-                <form id="login_form" class="form-signin" method="post">
+    <div class="card-body text-center">
+        <div class="admin-index-form-size">
+            <h3 class="form-signin-heading"><i class="fa-solid fa-lock"></i> Sign-Up </h3>
+            <form id="login_form" class="form-signin" method="post" action="/lmstle/signup_new.php">
 
-                    <div class="md-3">
-                        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Firstname"
-                            required>
-                    </div>
-                    <div class="md-3">
-                        <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Lastname"
-                            required>
-                    </div>
-                    <div class="md-3">
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Username"
-                            required>
-                    </div>
+                <input type="hidden" name="action" value="signup" />
 
-                    <div class="md-3">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Password"
-                            required>
-                    </div>
-                    <div class="md-3">
-                        <input type="password" class="form-control" id="cpassword" name="cpassword"
-                            placeholder="Confirm Password" required>
-                    </div>
+                <div class="md-3">
+                    <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Firstname"
+                        required>
+                </div>
+                <div class="md-3">
+                    <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Lastname"
+                        required>
+                </div>
+                <div class="md-3">
+                    <input type="text" class="form-control" id="username" name="username" placeholder="Username"
+                        required>
+                </div>
 
-                    <button id="back" type="button" class="btn btn-danger">
-                        <i class="fa-solid fa-arrow-rotate-backward"></i> Back
-                    </button>
+                <div class="md-3">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password"
+                        required>
+                </div>
+                <div class="md-3">
+                    <input type="password" class="form-control" id="cpassword" name="cpassword"
+                        placeholder="Confirm Password" required>
+                </div>
 
-                    <button id="signup" onclick="window.location='signup_new.php'" type="button"
-                        class="btn btn-info">
-                        <i class="fa-solid fa-sign-in"></i> Sign-Up
-                    </button>
+                <button id="back" type="button" class="btn btn-danger">
+                    <i class="fa-solid fa-arrow-rotate-backward"></i> Back
+                </button>
 
-                </form>
-            </div>
+                <button id="signup" type="submit" class="btn btn-info">
+                    <i class="fa-solid fa-sign-in"></i> Sign-Up
+                </button>
+
+            </form>
         </div>
-    </form>
+    </div>
 </div>
-<?php
-if (isset($_POST['save'])){
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$password = $_POST['password'];
 
+<?php 
+if (isset($_POST['action'])) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password = $_POST['password'];
 
-$query = mysqli_query($conn,"select * from teacher where username = '$username'")or die(mysqli_error());
-$count = mysqli_num_rows($query);
+    ($query = mysqli_query(
+        $conn,
+        "select * from teacher where username = '$username'"
+    )) or die(mysqli_error());
+    $count = mysqli_num_rows($query);
 
-if ($count > 0){ ?>
-   <script>
+    if ($count > 0) { ?>
+<script>
 alert('Data Already Exist');
-   </script>
-   <?php
-}else{
-mysqli_query($conn,"insert into teacher (username,password,firstname,lastname,location) values('$username','$password','$firstname','$lastname','uploads/NO-IMAGE-AVAILABLE.jpg')")or die(mysqli_error());
+</script>
+<?php } else {
+        mysqli_query(
+            $conn,
+            "insert into teacher (username,password,firstname,lastname,location) values('$username','$password','$firstname','$lastname','uploads/NO-IMAGE-AVAILABLE.jpg')"
+        ) or die(mysqli_error());
 
-mysqli_query($conn,"insert into activity_log (date,username,action) values(NOW(),'$username','Add User $username')")or die(mysqli_error());
-?>
-   <script>
-window.location = "admin/admin_user.php";
-   </script>
-   <?php
-}
-}
-?>
+        mysqli_query(
+            $conn,
+            "insert into activity_log (date,username,action) values(NOW(),'$username','Add User $username')"
+        ) or die(mysqli_error());
+        ?>
+<script>
+window.location = '/lmstle/signup_teacher.php';
+</script>
+<?php }
+} ?>
+
 <script>
 function handleBackNavigation() {
     window.location.href = '/lmstle/signup_teacher.php'
