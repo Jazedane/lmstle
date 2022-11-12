1f4eaff7012104e08d71d28fa2db08ecbf1baeff
24thsaint <rave.arevalo@kingsland.io>
2022-11-12 21:16:04 +0800

    feat(grade category): implement query and display

--- admin/category.php
+++ admin/category.php
@@ -5,12 +5,14 @@ $conn = $connector->databaseConnector();
 
 $category_name = $_POST['category_name'];
 $impact_percentage = $_POST['impact_percentage'];
+$class_id = $_POST['class_id'];
+$get_id = $_POST['get_id'];
 								
 $query = mysqli_query(
     $conn,
-    "INSERT INTO tbl_grade_category (category_name,class_id,category_name,impact_percentage) 
-                VALUES ('$session_id','$category_name','$class_id','$impact_percentage')"
-) or die(mysqli_error());
+    "INSERT INTO tbl_grade_category (category_name,class_id,impact_percentage) 
+                VALUES ('$category_name','$class_id','$impact_percentage')"
+) or die(mysqli_error($conn));
 
 ?>
 <script>
--- admin/grade-category.php
+++ admin/grade-category.php
@@ -24,20 +24,23 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <?php
-                                ($class_query = mysqli_query(
+                            ($class_query = mysqli_query(
                                 $conn,
                                 "SELECT * FROM tbl_teacher_class
 							    LEFT JOIN tbl_class ON tbl_class.class_id = tbl_teacher_class.class_id
 							    WHERE teacher_class_id = '$get_id'"
-                                )) or die(mysqli_error());
-                                $class_row = mysqli_fetch_array($class_query);
-                                $class_id = $class_row['class_id'];
-                        ?>
+                            )) or die(mysqli_error());
+                            $class_row = mysqli_fetch_array($class_query);
+                            $class_id = $class_row['class_id'];
+                            ?>
 
-                            <li class="breadcrumb-item"><a href="#"><?php echo $class_row['class_name']; ?></a> <span
-                                    class="divider"></span></li>
+                            <li class="breadcrumb-item"><a href="#"><?php echo $class_row[
+                                'class_name'
+                            ]; ?></a> <span class="divider"></span></li>
                             <li class="breadcrumb-item"><a href="#">School Year:
-                                    <?php echo $class_row['school_year']; ?></a> <span class="divider"></span></li>
+                                    <?php echo $class_row[
+                                        'school_year'
+                                    ]; ?></a> <span class="divider"></span></li>
                             <li class="breadcrumb-item active"><a href="#"><b>Grade Category</b></a></li>
                         </ol>
                     </div>
@@ -58,36 +61,83 @@
                                     </button>
                                 </div>
                             </div>
-                            <form class="" action="category.php" method="post"
-                                enctype="multipart/form-data" name="upload">
-                                <div class="card-body">
-                                    <h4>Grade Categories</h4><br>
-                                    <p>Grade categories must add up to 70% for performance task.</p>
+                            <div class="card-body">
+                                <h4>Grade Categories</h4><br>
+                                <p>Grade categories must add up to 70% for performance task.</p>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Grade Category</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Percentage</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <?php
+                                ($grade_category_query = mysqli_query(
+                                    $conn,
+                                    "SELECT * FROM tbl_grade_category
+                                    LEFT JOIN tbl_class ON tbl_class.class_id = tbl_grade_category.class_id
+                                    WHERE tbl_grade_category.class_id = '$class_id'"
+                                )) or die(mysqli_error());
+
+                                while (
+                                    $grade_category_row = mysqli_fetch_array(
+                                        $grade_category_query
+                                    )
+                                ) {
+
+                                    $category_name =
+                                        $grade_category_row['category_name'];
+                                    $impact_percentage =
+                                        $grade_category_row[
+                                            'impact_percentage'
+                                        ];
+                                    ?>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <?php echo $category_name; ?>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <?php echo $impact_percentage; ?>%
+                                    </div>
+                                </div>
+
+                                <?php
+                                    }
+                                ?>
+                                <form class="mt-5" action="category.php" method="post" enctype="multipart/form-data" name="upload">
+                                    <input type="hidden" name="class_id" value="<?php echo $class_id; ?>">
+                                    <input type="hidden" name="get_id" value="<?php echo $get_id; ?>">
+
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label>Grade Category</label>
                                                 <input type="text" name="category_name" class="form-control"
-                                                    placeholder="Enter name" required>
+                                                    placeholder="Enter new grade category name" required>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label>Percentage</label>
                                                 <input type="number" name="impact_percentage" maxlenth="3" max="100"
                                                     class="form-control" placeholder="Enter percentage" required>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <center>
-                                        <button class="btn btn-success" id="btn_add">Add Grade Category</button>
-                                        <input type="reset" class="btn btn-danger " id="reset" name="reset"
-                                            value="Cancel">
-                                    </center>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
+                            <div class="card-footer">
+                                <center>
+                                    <button class="btn btn-success" id="btn_add">Add Grade Category</button>
+                                    <input type="reset" class="btn btn-danger " id="reset" name="reset" value="Cancel">
+                                </center>
+                            </div>
                         </div>
                     </div>
                 </div>
