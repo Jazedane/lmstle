f7c665648789c3be74f76a8be783f5fa8286a944
Jazedane <juzztinemanapao27@gmail.com>
2022-12-04 16:35:41 +0800

    feat(admin): fix student side gallery

--- gallery.php
+++ gallery.php
@@ -33,8 +33,14 @@
             <div class="container-fluid">
                 <div class="row">
                     <?php
+
+                    $count_query = "SELECT * FROM image";
+                    $count_result = mysqli_query($conn, $count_query);
+                    $count = mysqli_num_rows($count_result);
+
                         $limit = 3;
-                        $page= $_REQUEST['page'];
+                        $max_pages = ceil($count / $limit);
+                        $page= $_REQUEST['current_page'];
                         $pages = $page-1;
                         $p = $pages * $limit;
 
@@ -68,10 +74,10 @@
 
                         <?php
                                 if($pages >= 1){
-                                echo "<a class='btn btn-success' href= ".$_SERVER['PHP_SELF']."?page=".($page - 1)."><i class='fas fa-arrow-left'></i> Prev</a>";
+                                echo "<a class='btn btn-success' href= ".$_SERVER['PHP_SELF']."?page=gallery&current_page=".($page - 1)."><i class='fas fa-arrow-left'></i> Prev</a>";
                              }
-                                if($page + 1 < $limit){
-                                echo "<a class='btn btn-success' href= ".$_SERVER['PHP_SELF']."?page=".($page + 1).">Next <i class='fas fa-arrow-right'></i></a>";
+                                if($pages < $max_pages-1){
+                                echo "<a class='btn btn-success ml-1' href= ".$_SERVER['PHP_SELF']."?page=gallery&current_page=".($page + 1)."> Next <i class='fas fa-arrow-right'></i></a>";
                               }
                             ?>
 
