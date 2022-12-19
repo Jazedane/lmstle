aa2d2e6a685aa9f528cb760a8bf6bc5db6c04e87
Jazedane <juzztinemanapao27@gmail.com>
2022-12-19 22:08:58 +0800

    feat(logout): deleted update logout date

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,6 @@
 <?php
 include('database.php');
 include('session.php');
-mysqli_query($conn,"update tbl_teacher_log set logout_Date = NOW() where teacher_id = '$session_id' ")or die(mysqli_error());
 
 session_destroy();
 header('location:/lmstlee4/login-page.php'); 
