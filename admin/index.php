a92a464b16c83761bd2ac90ffd9cba75368cd45c
Jazedane <juzztinemanapao27@gmail.com>
2022-12-31 22:55:37 +0800

    feat(admin): add new-dashboard

--- admin/index.php
+++ admin/index.php
@@ -53,12 +53,34 @@ function determine_active_sidebar_item($pages) {
         </nav>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <?php 
+                $query= mysqli_query($conn,"SELECT * FROM tbl_teacher WHERE teacher_id = '$session_id'")or die(mysqli_error());
+				$row = mysqli_fetch_array($query);
+                $is_superadmin = $row['is_superadmin'];
+
+            if ($is_superadmin == false) { ?>
             <a class="brand-link">
                 <img src="dist/img/logo.png" alt="BNHS Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">Admin/Teacher</span>
             </a>
+            <?php
+                }
+            ?>
+            <?php 
+                $query= mysqli_query($conn,"SELECT * FROM tbl_teacher WHERE teacher_id = '$session_id'")or die(mysqli_error());
+				$row = mysqli_fetch_array($query);
+                $is_superadmin = $row['is_superadmin'];
 
+            if ($is_superadmin == true) { ?>
+            <a class="brand-link">
+                <img src="dist/img/logo.png" alt="BNHS Logo" class="brand-image img-circle elevation-3"
+                    style="opacity: .8">
+                <span class="brand-text font-weight-light">Super Admin</span>
+            </a>
+            <?php
+                }
+            ?>
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <?php $query= mysqli_query($conn,"SELECT * FROM tbl_teacher WHERE teacher_id = '$session_id'")or die(mysqli_error());
@@ -88,7 +110,30 @@ function determine_active_sidebar_item($pages) {
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
+                        <?php 
+                            $query= mysqli_query($conn,"SELECT * FROM tbl_teacher WHERE teacher_id = '$session_id'")or die(mysqli_error());
+							$row = mysqli_fetch_array($query);
+                            $is_superadmin = $row['is_superadmin'];
 
+                        if ($is_superadmin == false) { ?>
+                        <li class="nav-item">
+                            <a href="new-dashboard.php"
+                                class="nav-link <?php echo determine_active_sidebar_item(['/lmstlee4/admin/new-dashboard.php']) ?>">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                        <?php
+                            }
+                        ?>
+                        <?php 
+                            $query= mysqli_query($conn,"SELECT * FROM tbl_teacher WHERE teacher_id = '$session_id'")or die(mysqli_error());
+							$row = mysqli_fetch_array($query);
+                            $is_superadmin = $row['is_superadmin'];
+
+                        if ($is_superadmin == true) { ?>
                         <li class="nav-item">
                             <a href="dashboard.php"
                                 class="nav-link <?php echo determine_active_sidebar_item(['/lmstlee4/admin/dashboard.php']) ?>">
@@ -98,6 +143,9 @@ function determine_active_sidebar_item($pages) {
                                 </p>
                             </a>
                         </li>
+                        <?php
+                            }
+                        ?>
                         <li class="nav-item">
                             <a href="class_main.php"
                                 class="nav-link <?php echo determine_active_sidebar_item(['/lmstlee4/admin/class_main.php']) ?>">
--- admin/new-dashboard.php
+++ admin/new-dashboard.php
@@ -32,27 +32,12 @@
             <div class="container-fluid">
                 <div class="row">
                     <?php
-                        ($query_teacher = mysqli_query(
-                        $conn,
-                        'select * from tbl_teacher where isDeleted=false'
-                        )) or die(mysqli_error());
-                        $count_teacher = mysqli_num_rows($query_teacher);
-                    ?>
-                    <div class="col-lg-6 col-6">
-                        <div class="small-box bg-primary">
-                            <div class="inner">
-                                <h3><?php echo $count_teacher; ?></h3>
-                                <p>Teachers</p>
-                            </div>
-                            <div class="icon">
-                                <i class="ion ion-person-stalker"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
                         ($query_student = mysqli_query(
                         $conn,
-                        'select * from tbl_student where isDeleted=false'
+                        "SELECT * FROM tbl_teacher_class_student
+                        LEFT JOIN tbl_student ON tbl_student.student_id = tbl_teacher_class_student.student_id
+                        INNER JOIN tbl_class ON tbl_class.class_id = tbl_student.class_id 
+                        WHERE teacher_id = '$session_id'"
                         )) or die(mysqli_error());
                         $count_student = mysqli_num_rows($query_student);
                     ?>
@@ -70,7 +55,10 @@
                     <?php
                         ($query_class = mysqli_query(
                         $conn,
-                        'select * from tbl_class where isDeleted=false'
+                        "SELECT * FROM tbl_teacher_class 
+                        INNER JOIN tbl_class ON tbl_class.class_id = tbl_teacher_class.class_id and tbl_teacher_class.school_year_id 
+                        WHERE teacher_id = '$session_id' 
+                        AND tbl_class.isDeleted = false"
                         )) or die(mysqli_error());
                         $count_class = mysqli_num_rows($query_class);
                     ?>
@@ -88,7 +76,9 @@
                     <?php
                         ($query_task = mysqli_query(
                         $conn,
-                        'select * from tbl_task where isDeleted=false'
+                        "SELECT * FROM tbl_task 
+                        LEFT JOIN tbl_grade_category ON tbl_grade_category.grade_category_id = tbl_task.grade_category_id
+                        WHERE tbl_task.class_id = '$session_id' AND isDeleted=false"
                         )) or die(mysqli_error());
                         $count_task = mysqli_num_rows($query_task);
                     ?>
